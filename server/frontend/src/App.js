fdd8b01f236878c74cca74e66d03e4057be06377
shumaria01 <shumaria.shahid01@gmail.com>
2025-07-14 15:38:27 -0400

    save changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-            <Route path="/dealers" element={<Dealers/>} />
+        <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
