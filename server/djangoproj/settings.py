129bbb91baedd748f27340a9affa2fdebf04ba0f
shumaria01 <shumaria.shahid01@gmail.com>
2025-07-15 08:10:58 -0400

    save chnages

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -1,9 +1,18 @@
-# SECURITY WARNING: keep the secret key used in production secret!
+INSTALLED_APPS = [
+    'djangoapp.apps.DjangoappConfig',  # your app
+    'django.contrib.admin',            # ✅ this is missing
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+
 SECRET_KEY = (
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 )
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -16,3 +25,5 @@ CSRF_TRUSTED_ORIGINS = [
     'https://shumariashah-8000.theiadockernext-1-labs-prod-theiak8s-4-'
     'tor01.proxy.cognitiveclass.ai',
 ]
+
+ROOT_URLCONF = 'djangoproj.urls'  # ✅ This is critical!
